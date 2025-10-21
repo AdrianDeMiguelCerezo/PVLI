59e092dd15ffd05f9b5f8259a2fc147640c35451
Adrixd112 <aerustes@ucm.es>
2025-10-22 00:57:09 +0200

    Cambios habilidad mal enfocada creo

--- src/habilidad.js
+++ src/habilidad.js
@@ -1,58 +1,54 @@
 import Jugador from "./jugador"
 
-const Target={
-    SELF,
-    ENEMY,
-    RNDENEMY,
-    ALLENEMIES
+const Target = {
+    SELF:'SELF',
+    ENEMY:'ENEMY',
+    RND_ENEMY:'RND_ENEMY',
+    ALL_ENEMIES:'ALL_ENEMIES'
 }
-const Tipo={
-    ACTIVA,
-    PASIVA,
-    TURNO1
+const Tipo = {
+    ACTIVA:'ACTIVA',
+    PASIVA:'PASIVA',
+    TURNO1:'TURNO1',
 }
 
-export class Habilidad extends Phaser.GameObjects{
-    constructor(Target,Tipo,id=0,nombre="",descripcion="",coste,damage,efecto){
-        super(Target,Tipo,id,nombre,descripcion,coste,damage,efecto)
-    }
-
-    Execute(){
-        switch(Tipo){
-            case ACTIVA:
-            if(SP>=coste){
-                switch(Target){
-                    case SELF:
-                        efecto.apply(Jugador)
-                        break
-                    case ENEMY:
-                        t=Combat.chooseTarget()
-                        t.Hit(damage)
-                        efecto.apply(t)
-                        break
-                    case RNDENEMY:
-                        t=Math.floor(Math.random*Combat.enemies.length)
-                        t.Hit(damage)
-                        efecto.apply(t)
-                        break
-                    case ALLENEMIES:
-                        for(e in Combat.enemies){
-                            t.Hit(damage)
-                            efecto.apply(t)
-                        }
-                        break
-                }
-            }
-                break
-            case PASIVA:
-                efecto.apply(Jugador)
-                break
-            case TURNO1:
-                if(Combate.turno=1){
-                    efecto.apply(Jugador)
-                }
-                break
-        }
+export class Habilidad {
+    constructor(Target, Tipo, id = 0, nombre = "", descripcion = "", coste, damage, efecto) {
         
     }
+
+    Execute(me) { //el combatManager ejecuta la habilidad y le pasa quién la está casteando.
+
+        objetivo;
+        switch (objetivo) {
+            //
+            case Target.SELF:
+                objetivo = me;
+                objetivo.Hit(damage);
+                objetivo.apply();
+                break;
+            case 'ENEMY':
+                objetivo = Combat.GetSelectedTarget();
+                objetivo.Hit(damage);
+                objetivo.apply();
+                break;
+            case 'RND_ENEMY':
+                objetivo = this.enemies[Math.floor(Math.random * this.enemies.length)];
+                objetivo.Hit(damage);
+                objetivo.apply();
+                break;
+            case 'ALL_ENEMIES':
+                for (e in Combat.enemies) {
+                    objetivo.Hit(damage)
+                    objetivo.apply()
+                }
+                break;
+        }
+
+
+
+    }
+
 }
+
+
