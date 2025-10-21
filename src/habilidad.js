ccd227444835c3b4deaf65b1e405f40b96fbddaf
Adrixd112 <aerustes@ucm.es>
2025-10-22 00:59:49 +0200

    comentarios

--- src/habilidad.js
+++ src/habilidad.js
@@ -22,22 +22,22 @@ export class Habilidad {
         objetivo;
         switch (objetivo) {
             //
-            case Target.SELF:
+            case Target.SELF: //lo aplico sobre mí
                 objetivo = me;
                 objetivo.Hit(damage);
                 objetivo.apply();
                 break;
-            case 'ENEMY':
+            case 'ENEMY': // lo aplico sobre un objetivo que me pasan (de la clase personaje)
                 objetivo = Combat.GetSelectedTarget();
                 objetivo.Hit(damage);
                 objetivo.apply();
                 break;
-            case 'RND_ENEMY':
+            case 'RND_ENEMY': // lo aplico sobre un objetivo random de los enemigos
                 objetivo = this.enemies[Math.floor(Math.random * this.enemies.length)];
                 objetivo.Hit(damage);
                 objetivo.apply();
                 break;
-            case 'ALL_ENEMIES':
+            case 'ALL_ENEMIES': // lo aplico sobre todos los enemigos
                 for (e in Combat.enemies) {
                     objetivo.Hit(damage)
                     objetivo.apply()
