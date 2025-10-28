dc43991e545870baee000ddaaeec064272b0b7ab
Adrixd112 <aerustes@ucm.es>
2025-10-28 19:26:32 +0100

    no guardé

--- src/Enemy.js
+++ src/Enemy.js
@@ -1,6 +1,8 @@
 export default class Enemy extends Phaser.GameObjects.Sprite {
-    constructor(key, scene, x, y, texture, frame) {
-        
+    constructor(key, scene, x, y, texture) {
+
+        super(scene, x, y, texture)
+
         this.key = key;
         this.Hp = 100; //poner la hp del key
         this.sigHabilidad = 0; //poner entero random entre 0 y el numero de habilidads.
--- src/scenes/BattleScene.js
+++ src/scenes/BattleScene.js
@@ -34,10 +34,6 @@ export default class BattleScene extends Phaser.Scene
 
 	}
 
-	init() {
-
-	}
-
 	create() {
 
 	}
