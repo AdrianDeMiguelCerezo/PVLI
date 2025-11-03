b4fc562858937cc2373cf0792b66285a4f827582
AdrianDeMiguelCerezo <adriadem@ucm.es>
2025-11-03 21:59:41 +0100

    game over

---
+++ src/scenes/GameOver.js
@@ -0,0 +1,41 @@
+export default class GameOver extends Phaser.Scene{
+    constructor(){
+        super({key:'GameOver'});
+    }
+
+    create(){
+        const { width, height } = this.scale;
+
+        this.add.rectangle(0, 0, width * 2, height * 2, 0x202040).setOrigin(0);
+
+        this.add.text(width / 2, height / 3 - 50, 'Game Over', {
+            fontFamily: 'Arial',
+            fontSize: '48px',
+            color: '#ffffff',
+            fontStyle: 'bold'
+        }).setOrigin(0.5);
+
+        const makeButton = (y, text, onClick) => {
+            const button = this.add.text(width / 2, y, text, {
+                fontFamily: 'Arial',
+                fontSize: '32px',
+                color: '#00ff99',
+                backgroundColor: '#00000055',
+                padding: { x: 20, y: 10 },
+                align: 'center'
+            })
+            .setOrigin(0.5)
+            .setInteractive({ useHandCursor: true });
+
+            button.on('pointerover', () => button.setStyle({ color: '#ffff00', backgroundColor: '#00000099' }));
+            button.on('pointerout', () => button.setStyle({ color: '#00ff99', backgroundColor: '#00000055' }));
+            button.on('pointerdown', onClick);
+
+            return button;
+        };
+
+        makeButton(height / 2, 'Return to start', () => {
+            this.scene.start('MainMenu');
+        });
+    }
+}
