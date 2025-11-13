81f0c0408b3c994ecec858ba461ecb4aade2f87c
Adrixd112 <aerustes@ucm.es>
2025-11-13 22:03:19 +0100

    Test color mapa

--- src/scenes/Map.js
+++ src/scenes/Map.js
@@ -107,31 +107,60 @@ export default class Map extends Phaser.Scene {
 
 
     //complejidad O(n^3) si eliminar un elemento de un array tiene complejidad O(n)
+    //GenerateDifficultyZones() {
+    //    let difficulty100nodes = []
+    //    let difficulty100arists = []
+    //    for (const node of this.nodes)
+    //    {
+    //        if (node.difficulty >= 100) { difficulty100nodes.push(node) }
+    //    }
+    //    for (const node of difficulty100nodes)
+    //    {
+    //        for (const other of this.nodes)
+    //        {
+    //            if (other.difficulty < 100 && Math.sqrt((node.x - other.x) * (node.x - other.x) + (node.y - other.y) * (node.y - other.y))<node.radius)
+    //            {
+    //                difficulty100arists.push(new Phaser.Math.Vector2(node.x + other.x, node.y + other.y).scale(0.5));
+    //            }
+    //        }
+    //    }
+    //    const path = new Phaser.Curves.Path();
+    //    path.lineTo(difficulty100arists[0]);
+    //    difficulty100arists.splice(0, 1)
+    //    console.log("arr: ",difficulty100arists)
+    //    for (let i = 0; i < difficulty100arists.length; i++)
+    //    {
+
+    //    }
+    //}
+
+
     GenerateDifficultyZones() {
-        let difficulty100nodes = []
-        let difficulty100arists = []
-        for (const node of this.nodes)
-        {
-            if (node.difficulty >= 100) { difficulty100nodes.push(node) }
-        }
-        for (const node of difficulty100nodes)
-        {
-            for (const other of this.nodes)
-            {
-                if (other.difficulty < 100 && Math.sqrt((node.x - other.x) * (node.x - other.x) + (node.y - other.y) * (node.y - other.y))<node.radius)
-                {
-                    difficulty100arists.push(new Phaser.Math.Vector2(node.x + other.x, node.y + other.y).scale(0.5));
+        for (let x = 0; x < this.game.canvas.width; x++) {
+            for (let y = 0; y < this.game.canvas.height; y++) {
+
+                let suma = 0;
+                let cantNodosCercanos = 0;
+
+                for (const n of this.nodes) {
+                    let d = Math.hypot([x - n.x, y - n.y]);
+                    if (d < n.radius) { suma += d; cantNodosCercanos++; }
+                }
+
+                const diffSum = cantNodosCercanos != 0 ? suma / cantNodosCercanos : 0
+
+                if (diffSum > 50) {
+                    this.drawPixel(x, y, 0x666666)
                 }
             }
         }
-        const path = new Phaser.Curves.Path();
-        path.lineTo(difficulty100arists[0]);
-        difficulty100arists.splice(0, 1)
-        console.log("arr: ",difficulty100arists)
-        for (let i = 0; i < difficulty100arists.length; i++)
-        {
-            
-        }
     }
+    // Create a Graphics object
 
+
+
+    drawPixel(x, y, color) {
+        this.areaGraphics.fillStyle(color);
+        this.areaGraphics.fillRect(x, y, 1, 1);
+    }
 }
