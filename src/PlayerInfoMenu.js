faae5a6346d0551f49b3042852757577e753a34d
AdrianDeMiguelCerezo <adriadem@ucm.es>
2025-11-19 11:37:52 +0100

    descripcion funciona

--- src/PlayerInfoMenu.js
+++ src/PlayerInfoMenu.js
@@ -127,10 +127,68 @@ export default class PlayerInfoMenu extends Phaser.GameObjects.Container
         }
     }
 
+    updateMenus(menuShow){
+        delete this.menuEquip;
+        delete this.menuItems;
+        delete this.menuHab;
+        delete this.menuDesc;
+        delete this.menuPlayer;
+        delete this.menuStats;
+
+        let w=this.scene.sys.canvas.width;
+        let h=this.scene.sys.canvas.height;
+
+        this.menuEquip = new Menu(this.scene, 20, 50, w/1.5, h * 0.8, 20, 3, 0x222222);
+        this.menuEquip.add(new Phaser.GameObjects.Text(this.scene, 0, 0, "Armas", { align: 'center' }));
+        this.menuEquip.add(new Phaser.GameObjects.Text(this.scene, (w / 1.5) * (1 / 3), 0, "Torso", { align: 'center' }));
+        this.menuEquip.add(new Phaser.GameObjects.Text(this.scene, (w / 1.5) * (2 / 3), 0, "Piernas", { align: 'center' }));
+
+        this.menuItems = new Menu(this.scene, 20, 50, w/1.5, h*0.8, 20, 3, 0x222222);
+        this.menuItems.add(new Phaser.GameObjects.Text(this.scene, 0, 0, "Fuera \nde combate", { align: 'center' }));
+        this.menuItems.add(new Phaser.GameObjects.Text(this.scene, (w / 1.5) * (1 / 3), 0, "Dentro \nde combate", { align: 'center' }));
+        this.menuItems.add(new Phaser.GameObjects.Text(this.scene, (w / 1.5) * (2 / 3), 0, "Fuera y dentro \nde combate", { align: 'center' }));
+
+        this.menuHab = new Menu(this.scene, 20, 50, w/1.5, h*0.8, 20, 3, 0x222222);
+
+        this.menuDesc=new Menu(this.scene,w*(2.1/3),50,w*(0.85/3),h*(0.8/3),3,2,0x222222);
+        this.menuDesc.add(new Phaser.GameObjects.Text(this.scene,0,0,this.desc));
+        this.menuDesc.AddButton(new MenuButton(this.scene,0,0,"Usar/equipar",null,()=>this.usar(clickedKey),15),2);
+        this.menuDesc.AddButton(new MenuButton(this.scene,0,0,"Desequipar",null,()=>this.desequipar(clickedKey),15),2);   
+
+        this.menuPlayer=new Menu(this.scene,w*(2.1/3),50+h*(0.86/3),w*(0.85/3),h*(0.6/3),6,1,0x222222);
+        this.menuPlayer.add(new Phaser.GameObjects.Text(this.scene,0,0,"Arma equipada: "));
+        this.menuPlayer.add(new Phaser.GameObjects.Text(this.scene,0,h*(0.6/3)*(1/6),this.arma));
+        this.menuPlayer.add(new Phaser.GameObjects.Text(this.scene,0,h*(0.6/3)*(2/6),"Torso equipado: "));
+        this.menuPlayer.add(new Phaser.GameObjects.Text(this.scene,0,h*(0.6/3)*(3/6),this.torso));
+        this.menuPlayer.add(new Phaser.GameObjects.Text(this.scene,0,h*(0.6/3)*(4/6),"Piernas equipadas: "));
+        this.menuPlayer.add(new Phaser.GameObjects.Text(this.scene,0,h*(0.6/3)*(5/6),this.pantalones)); 
+
+        this.menuStats=new Menu(this.scene,w*(2.1/3),60+h*(0.86/3)+h*(0.6/3),w*(0.85/3),h*(0.89/3),12,1,0x222222);
+        this.menuStats.add(new Phaser.GameObjects.Text(this.scene,0,0,"Defensa: "+this.def));
+        this.menuStats.add(new Phaser.GameObjects.Text(this.scene,0,h*(0.89/3)*(1/12),"Daño crítico: "+this.critDMG));
+        this.menuStats.add(new Phaser.GameObjects.Text(this.scene,0,h*(0.89/3)*(2/12),"Prob. crítica: "+this.critRate));
+        this.menuStats.add(new Phaser.GameObjects.Image(this.scene,20,h*(0.89/3)*(6/12),'player'));
+        this.menuStats.add(new Phaser.GameObjects.Text(this.scene,0,h*(0.89/3)*(8/12),"Dinero: "+this.dinero));
+        this.menuStats.add(new Phaser.GameObjects.Text(this.scene,0,h*(0.89/3)*(9/12),"HP: "+this.HP));
+        this.menuStats.add(new Phaser.GameObjects.Text(this.scene,0,h*(0.89/3)*(10/12),"SP: "+this.SP));
+        this.menuStats.add(new Phaser.GameObjects.Text(this.scene,0,h*(0.89/3)*(11/12),"Hambre: "+this.hambre));
+
+        this.addEquip();
+
+        if(menuShow==1){
+            this.showEquip();
+        }
+        else if(menuShow==2){
+            this.showItems();
+        }
+        else if(menuShow==3){
+            this.showHab();
+        }
+    }
+
     OnButtonClicked(key){
-        console.log("se hace")  
         this.desc=this.scene.jsonEquipamiento[key].description;
-        console.log(this.desc)
+        this.updateMenus(1);
     }
 
 }
