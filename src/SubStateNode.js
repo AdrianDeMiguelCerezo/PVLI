8bbcc98d1c813110cdf5490544b69b3a6443e39e
Adrixd112 <aerustes@ucm.es>
2025-11-26 00:27:25 +0100

    a medias generación de eventos (escena xd se usa para testear cosas de estas ahora)

--- src/SubStateNode.js
+++ src/SubStateNode.js
@@ -24,7 +24,40 @@ class SubStateNode {
         /**
          * @type {SubStateNode} subestado al que se va si se huye del combate.
          */
-        this.subState_Huida = subState_Huida;
+        this.EventFragment_Huida = subState_Huida;
+    }
+
+    /**
+     * 
+     * @param {object} combate
+     * @param {string} texto
+     * @param {{"text":string,"j":SubStateNode}[]} opciones
+     * @param {} consecuencias
+     * @param {SubStateNode} eventFragment_Huida
+     */
+    Create(combate, texto, opciones, consecuencias, eventFragment_Huida) {
+        this.combate = combate;
+
+        /**
+         * @type {string} texto del diálogo
+         */
+        this.texto = texto;
+
+        /**
+         * [{textoOpción,Nodo al q se va},{textoOpción,Nodo al q se va},...] 
+         * @type {{string,SubStateNode}[]}
+         */
+        this.opciones = opciones;
+
+
+        /**van los nombres de atributos del player y los cambios en estos {"dinero":... ,"items": ["",""],"equipamiento":["","",],"efectos":["",""],"habilidades":[],HP:... } si no hay cambio en un atributo, no se pone;
+         */
+        this.consecuencias = consecuencias;
+
+        /**
+         * @type {SubStateNode} subestado al que se va si se huye del combate.
+         */
+        this.EventFragment_Huida = eventFragment_Huida;
     }
 
 
--- src/scenes/xd.js
+++ src/scenes/xd.js
@@ -16,21 +16,57 @@ export default class xd extends Phaser.Scene {
     create() {
 
         console.log(1, this.jsonEventos)
-        this.generateEvent("PUEBLO_MEDIANO");
+        evento = this.generateEvent("PUEBLO_MEDIANO");
+        
 
 
     }
+
+    
+}
+
+class EventParser {
+    constructor(jsonEventos) {
+        this.jsonEventos = jsonEventos;
+        /**Guarda conversiones parámetroJson a atributo del PlayerData*/
+        this.rewardsJsonToAttribute =
+        {
+            "dinero":"dinero",
+            "HP": "HP",
+            "SP": "SP",
+            "hambre": "hambre",
+            "equipamiento": "equipamiento",
+            "habilidades": "habilidades",
+            "items": "items",
+            "efectos":"efectos"
+
+        }
+
+
+    }
+
 
     generateEvent(eventKey) {
 
         const eventoJson = this.jsonEventos[eventKey];
         let params = {};
-        params["aaaa"] = 1;
+        
         for (const par_nombre in eventoJson["params"]) {
 
             params[par_nombre] = this.GetJsonParamValue(eventoJson["params"][par_nombre]);
             console.log(eventoJson["params"][par_nombre]);
-            
+
+        }
+
+        let namedEventFragmentsArray = {}
+
+        for (const namedEventFragment in eventoJson["eventFragments"])
+        {
+            namedEventFragmentsArray[namedEventFragment] = new SubStateNode();
+        }
+        for(const namedEventFragment in eventoJson["eventFragments"])
+        {
+            this.GenerateNamedEventFragment(namedEventFragment, params, namedEventFragmentsArray)
         }
 
         console.log(params);
@@ -54,12 +90,31 @@ export default class xd extends Phaser.Scene {
             if (Array.isArray(infoGlobalParam)) {
                 return infoGlobalParam[Phaser.Math.RND.between(0, paramValue.length - 1)];
             }
-            else {return infoGlobalParam;}
+            else { return infoGlobalParam; }
         }
-        else {return paramValue;}
+        else { return paramValue; }
 
 
     }
 
+    /**
+     * @param {object} namedEventFragmentsArray Array con todos los fragmentos de evento para 
+     * @param {object} params guarda los parámetros del evento
+     * @param {object} namedEventFragment objeto que guarda la info del subevento a parsear del json.
+     * @returns {SubStateNode}
+     */
+    GenerateNamedEventFragment(namedEventFragment, params, namedEventFragmentsArray) {
+        
 
+        for(const eventFragment generate)
+
+
+
+
+        
+    }
+
+    GenerateSingleEventFragment() {
+
+    }
 }
