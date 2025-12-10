b5e760cdf666d1081f60be3917b70065aa373999
AdrianDeMiguelCerezo <adriadem@ucm.es>
2025-12-10 11:55:37 +0100

    evento final casi

--- src/scenes/xd.js
+++ src/scenes/xd.js
@@ -22,7 +22,7 @@ export default class xd extends Phaser.Scene {
 
         let eventParser = new EventParser(this.jsonEventos, this.jsonHabilidades, this.jsonEquipamiento, this.jsonItems, this.jsonEfectos)
         console.log("jsonEventos:", this.jsonEventos)
-        let evento = eventParser.generateEvent("PERRO_ATRAPADO");
+        let evento = eventParser.generateEvent("FINAL");
         console.log("evento:", evento)
 
         this.scene.start("DialogueScene", {fragmentoEvento: evento, playerData: new PlayerData()})
